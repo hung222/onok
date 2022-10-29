e1bc59ac1459d89dd13380035502038160937dea
hung222 <90371118+hung222@users.noreply.github.com>
2022-10-29 21:06:08 +0700

    Update index.js

--- kuju/routes/index.js
+++ kuju/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('trangchu', { title: 'trangchu' });
+  res.render('index', { title: 'index' });
 });
 
 module.exports = router;
